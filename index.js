43e344d937c5ce5824c6040dc1cbff2a34709795
Tam <mthanimr24@gmail.com>
2023-11-25 20:47:32 +0000

    seperated event handling to own file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, Events } = require('discord.js');
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -25,27 +25,17 @@ for (const folder of commandFolders) {
 	}
 }
 
-client.once(Events.ClientReady, () => {
-	console.log('Ready! ' + client.user.tag);
-});
+const eventsPath = path.join(__dirname, "events");
+const eventsFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
+for(const file of eventsFiles){
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if(event.once){
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
-});
+}
 
 client.login(process.env.TOKEN);
